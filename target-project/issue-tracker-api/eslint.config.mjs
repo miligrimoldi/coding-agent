aab300add4fc83a93f6100f11ca9fe36f7196727
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2026-07-17 16:36:26 -0300

    corrected lint

--- target-project/issue-tracker-api/eslint.config.mjs
+++ target-project/issue-tracker-api/eslint.config.mjs
@@ -6,7 +6,11 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+      ignores: [
+          'dist/**',
+          'coverage/**',
+          'src/generated/prisma/**',
+      ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
